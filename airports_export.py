c85d0d9a564ff0d714135ff447d9f19a62afd2af
ujazishere <ujasvaghani@gmail.com>
2024-03-30 18:58:07 -0400

    mdb fix

--- airports_export.py
+++ airports_export.py
@@ -2,7 +2,7 @@ import json
 import pickle
 import re
 
-airports = r'C:\Users\ujasv\OneDrive\Desktop\codes\Cirrostrats\cirrostrats-backend\all_US_airports_dict.pkl'
+airports = r'C:\Users\ujasv\OneDrive\Desktop\codes\cirrostrats-backend\all_US_airports_dict.pkl'
 with open(airports, 'rb') as f:
     x = pickle.load(f)
 
@@ -11,17 +11,27 @@ for a,b in x.items():
     cities = b[1]
     for y in cities:
         xx.append(y)
-test = {}
+
+airport_dict = {}
 for i in xx:
     
     first_dash_index = re.search(r'-', i).start()
     
     # Splitting the string by the first dash
-    first_part = i[:first_dash_index]
-    second_part = i[first_dash_index + 1:]
+    first_part = i[:first_dash_index][:-1]
+    second_part = i[first_dash_index + 1:][1:]
     
-    test.update({first_part:second_part})
+    airport_dict.update({first_part:second_part})
+
+airport_list = []
+for airport_id, airport_name in airport_dict.items():
+    airport_data = {
+        'id': airport_id,
+        'name': airport_name,
+        'code': airport_id  # Assuming the code is the same as the airport ID
+    }
+    airport_list.append(airport_data)
 
 
 with open('airports_dump.json','w') as f:
-    json.dump(test,f)
+    json.dump(airport_dict,f)
