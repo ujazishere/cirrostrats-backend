6f4a31cfaee9fca899e381685843386a6da70037
luisarevalo21 <arevaloluis89@yahoo.com>
2024-04-06 20:56:24 -0700

    adjusted the backend route to accept string

--- routes/route.py
+++ routes/route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from models.model import Flight, Airport
 from config.database import collection
-from schema.schemas import individual_serial, list_serial
+from schema.schemas import individual_serial, list_serial, individual_airport_input_data, serialize_airport_input_data
 from bson import ObjectId
 
 router = APIRouter()
@@ -24,8 +24,7 @@ router = APIRouter()
 
 @router.get('/airports')
 async def get_airports():
-
-    result = collection.find({})
+    result = collection.find({}).limit(5)
     return list_serial(result)
 
 
@@ -33,37 +32,58 @@ async def get_airports():
 # the id can be used to search for a specific airport
 # data returned is a dictionary wiht the id,name and code of the airport
 @router.get('/airports/{airportId}')
-async def get_airport_data(airportId):
+async def get_airport_data(airportId, search: str = None):
+
+    res = None
+    if (airportId == "airport"):
+        print("aiprotId is None")
+        print("inside if statement ")
+        res = collection.find({
+            "name": {"$regex": search}
+        })
+        return serialize_airport_input_data(res)
 
     res = collection.find_one(
         {"_id": ObjectId(airportId)})
-
     return individual_serial(res)
 
+    # return individual_serial(res)
 
-# print('airport', airport)
-# result = collection.find({})
-# return list_serial(result)
-# airports = []
-# cursor = collection.find({})
-# print("airports", airports)
-# airports['id'] = str(airports['_id'])
-# return await airports.to_list(length=None)
-# del [airports['_id']]
+    # res = collection.find_one(
+    #     {"_id": ObjectId(airportId)})
 
-# airports = []
-# cursor = collection.find({})
-# async for document in cursor:
-#     airports.append(individual_serial(document))
-# return airports
+    # return individual_serial(res)
 
+    # @router.get('/airports/filter/{airportString}')
+    # async def get_airport_data(airportString):
+    #     print('airportString', airportString)
+    #     res = collection.find({"name": {"$regex": airportString}})
+    #     if (res is None):
+    #         return []
+    #     return serialize_airport_input_data(res)
 
-# @router.post('/airports')
-# async def new_airport():
-# response = collection.find()
-# for airport in (await response.to_list(length=100)):
-#     airports.append(airport)
-# return response
+    # print('airport', airport)
+    # result = collection.find({})
+    # return list_serial(result)
+    # airports = []
+    # cursor = collection.find({})
+    # print("airports", airports)
+    # airports['id'] = str(airports['_id'])
+    # return await airports.to_list(length=None)
+    # del [airports['_id']]
 
-# flights = list_serial(collection.find())
-# return flights
+    # airports = []
+    # cursor = collection.find({})
+    # async for document in cursor:
+    #     airports.append(individual_serial(document))
+    # return airports
+
+    # @router.post('/airports')
+    # async def new_airport():
+    # response = collection.find()
+    # for airport in (await response.to_list(length=100)):
+    #     airports.append(airport)
+    # return response
+
+    # flights = list_serial(collection.find())
+    # return flights
