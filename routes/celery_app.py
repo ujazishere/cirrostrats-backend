a0734a4174e09f8aab2cec5c8ec45dc61047ff27
ujazishere <ujasvaghani@gmail.com>
2025-07-21 23:40:20 -0400

    gate fetch celery modification
    Ability to save to collection and clear historical

--- routes/celery_app.py
+++ routes/celery_app.py
@@ -2,7 +2,7 @@ import asyncio
 from celery import Celery
 from celery.schedules import crontab
 from routes.root.weather_fetch import Weather_fetch  # Used for periodic scheduling
-from routes.root.gate_scrape import Gate_Scrape
+from gate_processor import Gate_processor
 import datetime as dt
 
 '''     ***CAUTION***
@@ -51,9 +51,13 @@ async def run_TAF_fetch():
 
 @celery_app.task
 def GateFetch():
-    gs = Gate_Scrape()
+    gs = Gate_processor()
     gs.fetch_and_store()
 
+@celery_app.task
+def GateClear():
+    gs = Gate_processor()
+    gs.mdb_clear_historical(hours=3)
 
 celery_app.conf.timezone = 'UTC'  # Adjust to UTC timezone.
 
@@ -85,9 +89,16 @@ celery_app.conf.beat_schedule = {
         'schedule': crontab(minute=21, hour='5,11,17,23'),  # Run at 05:21, 11:21, 17:21, and 23:21 UTC
         # 'args': (16, 16)          # Arguments to pass to the task function
     },
-    'gatefetch-func_run_frequently': {
+    # TODO: Schedule this everyday at 1am/2am Eastern - Mind the zulu time.
+    'gateFetch-func_run_frequently': {
         'task': 'routes.celery_app.GateFetch',
-        'schedule': 1800,  # Every x seconds
+        # 'schedule': crontab(minute=35, hour='3'),     # test
+        'schedule': crontab(minute=59, hour='5'),  # Run at 05:59 UTC. 
+    },
+    'gateClear-func_run_frequently': {
+        'task': 'routes.celery_app.GateClear',
+        # 'schedule': crontab(minute=45, hour='3'),     # test
+        'schedule': crontab(minute=30, hour='6'),  # Run at 06:30 UTC. 
     },
     # uncomment the following if you need a function to run every x seconds. Change the task to its desired function.
     # 'func_run_frequently': {
