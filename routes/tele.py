b0f9027ef9de4e001f46730c5ac2cac4e1715216
ujazishere <ujasvaghani@gmail.com>
2025-08-20 14:25:52 -0400

    tele update w edct

--- routes/tele.py
+++ routes/tele.py
@@ -1,14 +1,23 @@
 import requests
 from decouple import config
 
+""" 
+    Telegram Bot API to send messages to a specific chat.
+    The bot token is stored in the .env file.
+    The chat ID is hardcoded for now, but can be changed - get it from the api. theyre unique to each chat/user
+    f'https://api.telegram.org/bot{TELE_BOT_TOKEN}/getUpdates'
+
+    """
 
 class Tele_bot:
     def __init__(self):
-        BOT_TOKEN = config("TELE_BOT_TOKEN")      # Telegram Bot Token from .env file
-        self.url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        self.CHAT_ID = "8483981154"
+        self.TELE_MAIN_BOT_TOKEN = config("TELE_MAIN_BOT_TOKEN")      # Telegram Bot Token from .env file
+        self.TELE_EWR_TOKEN = config("TELE_EWR_TOKEN")      # Telegram Bot Token from .env file
+        self.UJ_CHAT_ID = "8483981154"
+        self.ISMAIL_CHAT_ID = "763268014"      # Group chat ID - get it from the api.
 
-    def send_message(self, MESSAGE):
-        payload = {"chat_id": self.CHAT_ID, "text": MESSAGE}
+    def send_message(self, chat_id, MESSAGE,token):
+        self.url = f"https://api.telegram.org/bot{token}/sendMessage"
+        payload = {"chat_id": chat_id, "text": MESSAGE}
         r = requests.post(self.url, data=payload)
         print(r.json())
