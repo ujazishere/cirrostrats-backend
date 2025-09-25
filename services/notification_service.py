232c436969c4b557de803be67a84ae72531f6089
ujazishere <ujasvaghani@gmail.com>
2025-09-25 01:12:57 -0400

    basic notes and such

--- services/notification_service.py
+++ services/notification_service.py
@@ -2,7 +2,10 @@ from utils.tele import Tele_bot
 
 
 def send_telegram_notification_service(message):
-    
+    # TODO VHP: Maybe move this function to the core or within the class utils.tele?
+    # TODO LP: Setup continious loop to check for message on telegram for flight number request-
+            # When received send back updates - NAS status, weather changes, pdc, gate changes etc.
+            # This can serve as a proof concept.
     tb = Tele_bot()
 
     send_to = [tb.ISMAIL_CHAT_ID, tb.UJ_CHAT_ID]
